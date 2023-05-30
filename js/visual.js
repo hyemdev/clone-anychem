c5a0c0c0b7d8ecde3124f34903693591f017760a
hyemdev <devdev.hyem@gmail.com>
2023-05-30 18:04:18 +0900

    feat : visual swiper 기능개선 중

--- js/visual.js
+++ js/visual.js
@@ -1,43 +1,35 @@
-window.addEventListener("load", function () {
-    const swVisualWrap = this.document.querySelector(
-        ".sw-visual .swiper-wrapper"
-    );
 
-    //json 불러오기
-    const xhr = new XMLHttpRequest();
-    xhr.addEventListener("readystatechange", function (event) {
-        let req = event.target;
-        if (req.readyState === XMLHttpRequest.DONE) {
-            let data = JSON.parse(req.response);
-            makeVisualHtml(data);
-        }
+window.addEventListener("load", function (event) {
+  const swVisualWrap = document.querySelector(".sw-visual .swiper-wrapper");
+
+  fetch("data/visualdata.json")
+    .then((res) => res.json())
+    .then((result) => makeVisualHtml(result))
+    .catch((err) => console.log(err));
+
+  function makeVisualHtml(_data) {
+    let html = ``;
+    _data.img.forEach((item) => {
+      let temp = `
+            <div class="swiper-slide" style="background-image: url(images/${item})"></div>
+        `;
+      html += temp;
     });
-    xhr.open("GET", "data/visualdata.json");
-    xhr.send();
 
-    function makeVisualHtml(_data) {
-        let html = ``;
-        _data.img.forEach((item) => {
-            let temp = `
-                <div
-                class="swiper-slide"
-                style="background-image: url(images/${item})"></div>
-            `;
-            html += temp;
-        });
+    swVisualWrap.innerHTML = html;
 
-        //html에 밀어넣기
-        swVisualWrap.innerHTML = html;
-
-        //스와이퍼 넣는 위치 신경쓰기!
-        const swVisual = new Swiper(".sw-visual", {
-            loop: true,
-            effect: "fade",
-            speed: 800,
-            autoplay: {
-                delay: 3000,
-                disableOnInteraction: false,
-            },
-        });
-    }
+    const swVisual = new Swiper(".sw-visual", {
+      loop: true,
+      effect: "fade",
+      speed: 800,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".sw-visual-pg",
+        clickable: true,
+      },
+    });
+  }
 });
